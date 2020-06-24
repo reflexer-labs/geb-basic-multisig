c22728facaa96e2ec84f03b7db3eabe82f257984
stefanionescu <stefanionescu@protonmail.com>
2020-06-24 20:05:30 +0100

    Removed dummy multisig with delay

--- src/MultisigWallet.sol
+++ src/MultisigWallet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.15;
+pragma solidity ^0.6.7;
 
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
